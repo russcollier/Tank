2a889244ed256617832822992db7c4128bd91aba
DevOps On Windows <russ.collier@gmail.com>
2014-10-28 18:36:47 -0500

    Header/project info

---
+++ lib/tank/__init__.py
@@ -0,0 +1,8 @@
+__author__ = "Russ Collier"
+__copyright__ = "Copyright 2014, Russ Collier"
+__credits__ = ["Russ Collier"]
+__license__ = "MIT"
+__version__ = "1.0.0"
+__maintainer__ = "Russ Collier"
+__email__ = "russ.collier@gmail.com"
+__status__ = "Development"
